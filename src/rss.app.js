80197ee3ceb5c151c0d55530367f3805077e8ed2
Levashov Kirill <klevashov@htsts.ru>
2020-09-04 10:09:44 +0300

    refactoring code how to add new links

--- src/rss.app.js
+++ src/rss.app.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import $ from 'jquery';
-import { updateChannels, getLatestItemDate } from './rss.chanels';
+import { flatten, maxBy } from 'lodash';
 import isValid from './validator';
 import parseChannel from './parser';
 import { renderAlert } from './renderers';
@@ -45,6 +45,8 @@ export default () => {
     states.formState = isValid(states) ? 'valid' : 'invalid';
   });
 
+  const getLatestItemDate = (items) => maxBy(items, 'pubDate').pubDate;
+
   button.addEventListener('click', () => {
     const feed = states.input;
     states.formState = 'loading';
@@ -69,6 +71,43 @@ export default () => {
       button.click();
     }
   });
+
+  const getNewChannelItems = ({ items }, i) => {
+    const latestItemDate = getLatestItemDate(items);
+    const channel = states.channels[i];
+    if (latestItemDate <= channel.latestItemDate) {
+      return [];
+    }
+    const newChannelItems = items.filter(({ pubDate }) => (
+      pubDate > channel.latestItemDate));
+    newChannelItems.forEach((item) => {
+      // eslint-disable-next-line no-param-reassign
+      item.channelId = channel.channelId;
+    });
+    states.channels[i].latestItemDate = latestItemDate;
+    return newChannelItems;
+  };
+
+  const updateChannels = () => {
+    const { channels } = states;
+    const requests = channels.map(({ channelFeed }) => axios.get(channelFeed));
+    axios.all(requests)
+      .then((responses) => {
+        const newChannels = responses.map((response) => parseChannel(response.data));
+        const newItems = newChannels.map(getNewChannelItems);
+        const itemsToUpdate = flatten(newItems);
+        if (itemsToUpdate.length > 0) {
+          states.toUpdate = itemsToUpdate;
+        }
+      })
+      .catch((error) => {
+        states.error = error;
+        states.formState = 'error';
+      })
+      .finally(() => setTimeout(updateChannels, 5000));
+  };
+
   watchers(formStateMethods, states);
+
   updateChannels(states);
 };
